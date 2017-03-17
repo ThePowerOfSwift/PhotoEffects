cb6c58e11b00e01d6954f1ef07337aa644cb44a3
Aleksandr Vasilyev <One@Ones-MacBook-Pro.local>
2017-03-17 21:37:06 +0300

    Initial Commit

---
+++ PhotoEffects/PhotoListController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoEffects
+//
+//  Created by One on 17/03/2017.
+//  Copyright © 2017 One. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
